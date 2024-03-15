ab73777bca89a3c7c896a1e2a1c6f334c7c28882
Samira Chetta <chettasamira@gmail.com>
2024-03-15 13:33:00 -0400

    Implement asset caching in src-sw.js

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+   // Filter the requests we want to cache
+   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+   new StaleWhileRevalidate({
+     cacheName: 'asset-cache',
+     plugins: [
+       new CacheableResponsePlugin({
+         statuses: [0, 200],
+       }),
+     ],
+   })
+);
