08e616d7c106817060eedafb82a202aab9acd03e
Samira Chetta <chettasamira@gmail.com>
2024-03-15 12:17:25 -0400

    Added and configured workbox plugins for a service worker and a manifest file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,11 +16,40 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
+// TODO: Add and configure workbox plugins for a service worker
+  //service worker
+  new InjectManifest({
+    swSrc: './src-sw.js',
+    swDest: 'src-sw.js'
+  }),
+  // TODO: Add and configure workbox plugins for a manifest file
+  //manifest.json
+  new WebpackPwaManifest({
+    fingerprints: false,
+    inject: true,
+    name: 'Just Another Text Editor',
+    short_name: 'J.A.T.E',
+    description: 'A way to take notes or keep track of tasks!',
+    background_color: '#0F111A',
+    theme_color: '#0F111A',
+    start_url: './',
+    publicPath: './',
+    icons: [
+      {
+        src: path.resolve('src/images/logo.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join('assets', 'icons'),
+      },
     ],
+  })
+  ],
 
     module: {
-      rules: [
+      rules: [ 
         
       ],
     },
