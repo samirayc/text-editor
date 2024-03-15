a370ca2869e368ca864d104a12fea04996f58957
Samira Chetta <chettasamira@gmail.com>
2024-03-15 12:44:14 -0400

    Add putDb method to accept and add content to database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,20 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+  console.error('putDb not implemented');
+  console.log('PUT to the database');
+  // Connect to DB
+  const textDb = await openDB('text', 1);
+  // Make new transaction, specify the db being posted to
+  const tx = textDb.transaction('text','readwrite');
+  // Open object store
+  const store = tx.objectStore('text');
+  // Use put method to pass in content
+  const request = store.put({ text: content });
+  // Confirm data was added
+  const result = await request; console.log('Data saved to the database!', result)
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
